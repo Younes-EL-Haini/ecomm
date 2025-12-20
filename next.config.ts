8bfaa50db4fe1242351d447d9035570c9f57d806
Younes-EL-Haini <elhaini.youness@gmail.com>
2025-12-20 14:01:43 +0100

    Adding Dummy images

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,35 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: 'res.cloudinary.com',
+//         pathname: '/**', // Allows all images from this host
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: 'res.cloudinary.com',
-        pathname: '/**', // Allows all images from this host
+        protocol: 'https',
+        hostname: 'placehold.co', // The domain from your error log
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dummyimage.com', // Keeping this one just in case
+        port: '',
+        pathname: '/**',
       },
     ],
   },
