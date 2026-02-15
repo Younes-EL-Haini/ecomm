56a7552b07674018ca73cfd1d9d6352bab596f87
Younes-EL-Haini <elhaini.youness@gmail.com>
2026-02-15 21:41:43 +0100

    Fixing non existing images

--- components/products/ProductImage.tsx
+++ components/products/ProductImage.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 type ProductImageCardProps = {
   image?: { url: string; alt?: string | null };
@@ -6,10 +8,16 @@ type ProductImageCardProps = {
 };
 
 const ProductImageCard = ({ image, stock }: ProductImageCardProps) => {
+  const [imgSrc, setImgSrc] = useState(
+    image?.url || "/images/hero-product.jpg",
+  );
+  useEffect(() => {
+    setImgSrc(image?.url || "/images/hero-product.jpg");
+  }, [image?.url]);
   return (
     <div className="relative w-full aspect-4/5 overflow-hidden bg-zinc-100">
       <Image
-        src={image?.url || "/images/hero-product.jpg"}
+        src={imgSrc}
         alt={image?.alt || "Product image"}
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
